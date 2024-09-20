7a5ed8e479f3df4a10c6e7bd9f70eb65b57872d7
Kayky <kayky159@gmail.com>
2024-09-20 00:48:36 -0300

    Initial commit

---
+++ SistemaPadaria/Controllers/ProdutosController.cs
@@ -0,0 +1,30 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SistemaPadaria.Models;
+
+namespace SistemaPadaria.Controllers;
+
+public class ProdutosController : Controller
+{
+    public IActionResult Cadastrar()
+    {
+        return View();
+    }
+
+ public IActionResult Alterar()
+    {
+        return View();
+    }
+
+ public IActionResult Remover()
+    {
+        return View();
+    }
+
+ public IActionResult Listar()
+    {
+        return View();
+    }
+
+   
+}
