0a9b0bec38a78ea845ba99e3931f2c14bcf345db
Kayky Felipe do Nascimento <kayky159@gmail.com>
2024-10-24 17:07:09 -0300

    Commit

--- SystemMarket/Repositorio/Produtos Repositorio/ProdutoRepositorio.cs
+++ SystemMarket/Repositorio/Produtos Repositorio/ProdutoRepositorio.cs
@@ -5,7 +5,7 @@ using SystemMarket.Models;
 namespace SystemMarket.Repositorio
 {
 
-public class ProdutoRepositorio : IProdutoRepositorio
+    public class ProdutoRepositorio : IProdutoRepositorio
     {
         private readonly BancoContext _bancoContext;
 
@@ -16,10 +16,10 @@ public class ProdutoRepositorio : IProdutoRepositorio
         }
         public ProdutoModel Adicionar(ProdutoModel produtos)
         {
-           _bancoContext.Set<ProdutoModel>().Add(produtos); //salvar no banco de dados 
-           _bancoContext.SaveChanges();  
+            _bancoContext.Set<ProdutoModel>().Add(produtos); //salvar no banco de dados 
+            _bancoContext.SaveChanges();
 
-           return produtos;
+            return produtos;
         }
 
         public List<ProdutoModel> BuscarProdutos()
@@ -27,17 +27,21 @@ public class ProdutoRepositorio : IProdutoRepositorio
             return _bancoContext.Set<ProdutoModel>().ToList();
         }
 
-        public ProdutoModel ListarProduto(int id)
-        {          
 
-            return  _bancoContext.Produtos.FirstOrDefault(x => x.Id == id);
+
+        public ProdutoModel ListarProduto(int id)
+        {
+
+            return _bancoContext.Produtos.FirstOrDefault(x => x.Id == id);
         }
+
+
 
         public ProdutoModel Atualizar(ProdutoModel produto)
         {
             ProdutoModel produtoDB = ListarProduto(produto.Id);
 
-            if(produtoDB == null)throw new Exception("Houve um erro na atualização do produto!!");
+            if (produtoDB == null) throw new Exception("Houve um erro na atualização do produto!!");
             produtoDB.CodigoBarras = produto.CodigoBarras;
             produtoDB.Nome = produto.Nome;
             produtoDB.Valor = produto.Valor;
@@ -46,29 +50,29 @@ public class ProdutoRepositorio : IProdutoRepositorio
             _bancoContext.Produtos.Update(produtoDB);
             _bancoContext.SaveChanges();
 
-            return produtoDB; 
+            return produtoDB;
 
 
-            
+
         }
 
         public bool Remover(int id)
         {
             ProdutoModel produtoDB = ListarProduto(id);
 
-            if(produtoDB == null)throw new Exception("Houve um erro na Remoção do produto!!");
+            if (produtoDB == null) throw new Exception("Houve um erro na Remoção do produto!!");
 
 
             _bancoContext.Produtos.Remove(produtoDB);
             _bancoContext.SaveChanges();
-            
+
             return true;
 
 
-            
+
         }
 
-       
+
     }
 
 
