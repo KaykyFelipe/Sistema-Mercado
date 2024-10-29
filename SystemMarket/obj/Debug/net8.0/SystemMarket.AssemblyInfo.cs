9c3455f7bde56e249e20d6f1151b9ea570195a65
Kayky Felipe do Nascimento <kayky159@gmail.com>
2024-10-29 17:24:45 -0300

    Commit

--- SystemMarket/obj/Debug/net8.0/SystemMarket.AssemblyInfo.cs
+++ SystemMarket/obj/Debug/net8.0/SystemMarket.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SystemMarket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+443532b55dcd77b9d615b08c3da3dccf9925ef29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a9b0bec38a78ea845ba99e3931f2c14bcf345db")]
 [assembly: System.Reflection.AssemblyProductAttribute("SystemMarket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SystemMarket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
